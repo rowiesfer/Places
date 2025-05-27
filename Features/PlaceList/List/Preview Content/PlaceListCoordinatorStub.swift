ec0441bdee4a87cca7e8055d7c27ca5293967baa
rowiesfer <jfrowies@gmail.com>
2025-05-27 22:18:40 +0200

    Re-organise folder structure

--- Features/PlaceList/List/Preview Content/PlaceListCoordinatorStub.swift
+++ Features/PlaceList/List/Preview Content/PlaceListCoordinatorStub.swift
@@ -5,6 +5,7 @@
 //  Created by Fer on 27/05/2025.
 //
 
+
 final class PlaceListCoordinatorStub: PlaceListCoordinatorProtocol {
     func showError(localizedMessage: String) { }
     func showCustomPlace() { }
