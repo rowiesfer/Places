c3cc3dc6a93946b0374ac01dcfc751663cb38bd0
rowiesfer <jfrowies@gmail.com>
2025-05-28 17:22:01 +0200

    Use nil to better represent a place without name

--- Features/PlaceList/List/Preview Content/PlaceListRepositoryStub.swift
+++ Features/PlaceList/List/Preview Content/PlaceListRepositoryStub.swift
@@ -15,7 +15,7 @@ public struct PlaceListRepositoryStub: PlaceListRepositoryProtocol {
             .init(name: "Amsterdam", latitude: 52.3547498, longitude: 4.8339215),
             .init(name: "Mumbai", latitude: 19.0823998, longitude: 72.8111468),
             .init(name: "Copenhagen", latitude: 55.6713442, longitude: 12.523785),
-            .init(name: "", latitude: 40.4380638, longitude: -3.7495758)
+            .init(name: nil, latitude: 40.4380638, longitude: -3.7495758)
         ]
     }
 }
